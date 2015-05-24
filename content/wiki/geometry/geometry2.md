2fd08a5709e59ccc69a78d1818a3e74525fe4642
pgruenbacher <pgruenbacher@gmail.com>
2015-05-23 21:53:54 -0400

    collapsible

---
+++ content/wiki/geometry/geometry2.md
@@ -0,0 +1,10 @@
++++
+Categories = ["Development"]
+Description = "Description appears here"
+Tags = ["cfd"]
+date = "2015-05-23T21:51:49-04:00"
+menu = "main"
+title = "geometry2"
+
++++
+
--- static/js/site.js
+++ static/js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Variables
-    var $popoverLink = $('[data-popover'])
+    var $popoverLink = $('[data-popover]')
 
     function init(){
         $popoverLink.on('click',openpopover)
@@ -9,7 +9,7 @@ $(document).ready(function(){
     function openPopover(e){
         e.preventDefault()
         closePopover()
-        var popover $($(this).data('popover'));
+        var popover = $(this).data('popover');
         popover.toggleClass('open')
         e.stopImmediatePropagation()
     }
@@ -17,6 +17,32 @@ $(document).ready(function(){
     function closePopover(e){
         if($('.popover.open').length > 0) {
           $('.popover').removeClass('open')
-      }
-  }
-})
+        }
+    }
+
+    //sidebar dropdown menu
+    $('#sidebar .sub-menu > a').click(function () {
+        console.log('click submenu a')
+        // Close previous open submenu
+        var last = jQuery('.sub.open', jQuery('#sidebar'));
+        jQuery(last).slideUp(200);
+        jQuery(last).removeClass("open");
+        jQuery('.menu-arrow', jQuery(last).parent()).addClass('fa-angle-right');
+        jQuery('.menu-arrow', jQuery(last).parent()).removeClass('fa-angle-down');
+
+        // Toggle current submenu
+        var sub = jQuery(this).next();
+        if (sub.is(":visible")) {
+            jQuery('.menu-arrow', this).addClass('fa-angle-right');
+            jQuery('.menu-arrow', this).removeClass('fa-angle-down');
+            sub.slideUp(200);
+            jQuery(sub).removeClass("open")
+        } else {
+            jQuery('.menu-arrow', this).addClass('fa-angle-down');
+            jQuery('.menu-arrow', this).removeClass('fa-angle-right');
+            sub.slideDown(200);
+            jQuery(sub).addClass("open")
+        }
+
+    });
+});
